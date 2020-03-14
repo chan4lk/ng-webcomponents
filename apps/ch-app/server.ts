d01f87bd99ef3dfd9a5a5ad1aa894667f7f19d81
Chandima Ranaweera <chan4lk@gmail.com>
2020-03-15 01:08:04 +0530

    fix ssr

--- apps/ch-app/server.ts
+++ apps/ch-app/server.ts
@@ -11,7 +11,7 @@ import { existsSync } from 'fs';
 // The Express app is exported so that it can be used by serverless Functions.
 export function app() {
   const server = express();
-  const distFolder = join(process.cwd(), 'dist/ch-app/browser');
+  const distFolder = join(process.cwd(), 'dist/apps/ch-app/browser');
   const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
 
   // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
@@ -31,7 +31,7 @@ export function app() {
 
   // All regular routes use the Universal engine
   server.get('*', (req, res) => {
-    res.sendFile(indexHtml, { req, providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }] });
+    res.render(indexHtml, { req, providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }] });
   });
 
   return server;
