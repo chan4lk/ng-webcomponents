c1c0e5112465cf88832c57c43f51ce0ac15a0cec
Chandima Ranaweera <chan4lk@gmail.com>
2020-03-15 00:10:59 +0530

    move ch-components

--- apps/ch-app/src/app/dashboard/dashboard.module.ts
+++ apps/ch-app/src/app/dashboard/dashboard.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
-import { MessageCardModule } from '@chan4lk/ch-components';
+import { MessageCardModule, ChComponentsModule } from '@chan4lk/ch-components';
 import { HomeComponent } from './home/home.component';
 import { StoreModule } from '@ngrx/store';
 import { reducer, featureKey } from './+state/dashboard.reducer';
 import { EffectsModule } from '@ngrx/effects';
 import { DashboardEffects } from './+state/dashboard.effects';
-import { ChComponentsModule } from 'projects/ch-components/src';
 import { MessagesComponent } from './messages/messages.component';
 import { AboutComponent } from './about/about.component';
 
